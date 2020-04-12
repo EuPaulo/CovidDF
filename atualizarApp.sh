5af6442f59c99ee1ee22f57dd989ca2704c50a9e
Paulo <paulomiramor@gmail.com>
2020-04-12 11:00:47 -0300

    Atualizado em

---
+++ atualizarApp.sh
@@ -0,0 +1,7 @@
+GIT=`which git`
+REPO_DIR=~/Dropbox/CBMDF/Covid/dadosCovidDF
+cd ${REPO_DIR}
+Rscript geraCsv.R
+${GIT} add . -A
+${GIT} commit -m "Atualizado em  `date+'%d-%m-%Y %H:%M:%S'`"
+${GIT} push
