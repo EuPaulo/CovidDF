cc94dc35354339e0d7bcdf8d755d5587670c12ed
Paulo <paulomiramor@gmail.com>
2020-04-12 13:13:13 -0300

    Atualizado em  12-04-2020 13:13:09

--- geraCsv.R
+++
@@ -1,8 +0,0 @@
-library(googlesheets4)
-
-
-planilha <- read_sheet('https://docs.google.com/spreadsheets/d/1Nc_JFHnVtLcxzY2sMft4oo9tvVWKy0pd7r86ozuNm5w/edit#gid=0')
-
-write.csv(planilha, file = 'boletimDF.csv', row.names = FALSE)
-
-print("Planilha escrita")
